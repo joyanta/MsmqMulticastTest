6712dd03fd1d043473123b1da3392d50ed75306e
joyanta <joyanta.uk@gmail.com>
2016-09-12 11:00:09 +0100

    first commit

---
+++ MsmqMulticastTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Messaging;
+
+namespace MsmqMulticastTest.Subscriber2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var queue = new MessageQueue(".\\private$\\subscriber1"))
+            {
+                queue.MulticastAddress = "234.1.1.1:8001";
+                while (true)
+                {
+                    var message = queue.Receive();
+                    var reader = new StreamReader(message.BodyStream);
+                    Console.WriteLine(string.Format("Received: {0}", reader.ReadToEnd()));
+                }
+
+            }
+        }
+    }
+}
